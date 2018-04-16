d9bc3a6295c46fa09ff2348e2f39a305b31addf1
Hyunmin Joo <jooh2@uw.edu>
2018-04-16 16:49:31 -0700

    working with flight information

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -9,36 +9,54 @@
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
+new_flights <- data.frame(flights, stringsAsFactors = FALSE)
+dim(flights)
+colnames(new_flights)
 
-
-# Use `dplyr` to give the data frame a new column that is the amount of time
+# Use `dplyr` to give the new_flights frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- flights %>%
+  mutate(gain_in_air = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- flights %>%
+  arrange(-gain_in_air)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+flights <- flights %>%
+  mutate(gain_in_air = arr_delay - dep_delay)%>%
+  arrange(-gain_in_air)
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(new_data %>%
+       select(time)%>%
+       filter(time > 0)
+     )
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+# mean(flights$gain_in_air, na.rm = TRUE)
+summarise(flights, mean = mean(flights$gain_in_air, na.rm = TRUE))
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
+to_sea <- data %>% 
+  filter(dest == "SEA") %>%
+  select(origin, dest, gain_in_air)
 
 # On average, did flights to SeaTac gain or loose time?
-
+summarise(to_sea, mean = mean(gain_in_air, na.rm = TRUE))
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
-
+summary <- flights %>%
+  filter(origin == "JFK", dest == "SEA") %>%
+  summarise(
+    avg_time = mean(gain_in_air, na.rm = T),
+    min_time = min(gain_in_air, na.rm = T),
+    max_time = max(gain_in_air, na.rm = T)
+  )
